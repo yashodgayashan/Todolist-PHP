c1aedd985b9c98b835a9d70649d20ebe55674be9
yashodgayashan <yashodgayashan@gmail.com>
2020-05-01 11:18:15 +0530

    Added create user sql file

---
+++ resources/create_users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
